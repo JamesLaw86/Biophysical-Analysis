a0331a17dd067a9d7eb818c1fd7c524718b0970d
James Law <james.law@apltd.co.uk>
2017-09-28 18:10:56 +0100

    Moving directories around a bit

---
+++ biophysical_analysis/PhaseDiagrams/EPD.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Class for creating Empirical Phase Diagrams
+
+"""
+import sys
+sys.path.append('../')
+from DataProcessing import data_process
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+class EPD(object):
+    
+
